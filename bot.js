33496f442d605f9479ab15edd13db68f2c98fd0c
elizabethMiranda <mirandaeli23@gmail.com>
2023-11-30 14:34:45 -0600

    fix bug

--- bot.js
+++ bot.js
@@ -1,32 +1,10 @@
 //const { Telegraf } = require("telegraf");
 require('dotenv').config()
-const Telegram = require('telegram-node-bot');
 const Consulta = require("./models/consultas");
 const Server = require("./models/server");
-//const tg = new Telegram.Telegram('6950681715:AAHgqAU7MosRkf_G9X-GWGTHC4qlX3dK3zU')
-//const bot = new Telegraf('6950681715:AAHgqAU7MosRkf_G9X-GWGTHC4qlX3dK3zU');
 const getconsulta = new Consulta();
 
 
 
 const server = new Server();
 server.listen();
-
-// tg.router
-//     .when(
-//         new TextCommand('/start', 'startHandler'),
-//         new StartController()
-//     )
-
-// tg.sendMessage('Hello!','6950681715:AAHgqAU7MosRkf_G9X-GWGTHC4qlX3dK3zU','-1002023907305')
-
-
-// bot.start((ctx) => {
-//   ctx.reply("Welcome");
-// });
-
-// bot.command("mycommand", (ctx) => {
-//   ctx.reply("my custom command");
-// });
-
-// bot.launch();
