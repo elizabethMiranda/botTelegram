ae577cb1b32719f238c327ac14c796a77bed06ea
elizabethMiranda <mirandaeli23@gmail.com>
2023-12-03 14:16:13 -0600

    cambios en variable de telegram

--- models/server.js
+++ models/server.js
@@ -13,14 +13,15 @@ class Server{
         this.middlewares();
         //Rrutas de mi aplicacion
         this.routes();
+        this.token = process.env.TELEGRAM_TOKEN;
 
     }
 
     routes(){
         this.app.get('/mensaje', async(req, res) => {
             try{
-                const tg = new Telegram.Telegram(process.env.TELEGRAM_TOKEN)
-                const results = await tg.api.sendMessage('','Four message!',process.env.TELEGRAM_TOKEN,'HTML');
+                const tg = new Telegram.Telegram(this.token)
+                const results = await tg.api.sendMessage('-1002023907305','Four message!',process.env.TELEGRAM_TOKEN,'HTML');
                 const { _messageId, _text } = results;
                 console.log('datos de messageid');
                 console.log(_messageId);
@@ -31,17 +32,37 @@ class Server{
                 throw err;
             }
           })
-        
           this.app.get('/update-mensaje', async(req, res) => {
             try{
                 const tg = new Telegram.Telegram(process.env.TELEGRAM_TOKEN)
                 let chatId={
-                    chat_id:'',
-                    message_id:0,
+                    chat_id:'-1002023907305',
+                    message_id:25,
                 }
                 const resultsEd = await tg.api.editMessageText('Four message editado!',chatId);
                 //const { _messageId, _text } = results;
                 console.log('datos de messageid');
+                console.log(resultsEd);
+                //console.log('Texto enviado');
+                //console.log(_text);
+            }catch(err){
+                console.log('Error al enviar mensaje', err);
+                throw err;
+            }
+          })
+          this.app.get('/update-reaction', async(req, res) => {
+            try{
+                const tg = new Telegram.Telegram(process.env.TELEGRAM_TOKEN)
+                let chatId={
+                    chat_id:'-1002023907305',
+                    message_id:25,
+                }
+                const resultsEd = await tg.api.sendReaction(msg_id,{
+                    type:'😍'
+                });
+                //('Four message editado!',chatId);
+                //const { _messageId, _text } = results;
+                console.log('datos de sendReaction');
                 console.log(resultsEd);
                 //console.log('Texto enviado');
                 //console.log(_text);
